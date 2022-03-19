46dfcdb1340648ac076506f45845e298403d96cf
Paweł Kopel <pawel.kopel2@gmail.com>
2022-03-19 20:19:13 +0100

    Updates from 19-3-2022

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 Scripts downloading air quality data from [Airly](https://developer.airly.org/en).
 
 ## Usage
-1. Create file `Airlty_API.txt` containing your Airly API key
+1. Create file `Airly_API.json` containing your Airly API key(s) in a json array (eg. `[ "<API key>" ]`)
 2. Save list of sensors to query in `sensors_list.json` (as a JSON list of objects with at least `'id'` field)
 3. Run `python get_measurements.py` to download and save current measurmenets
--- get_measurements.py
+++ get_measurements.py
@@ -7,8 +7,9 @@ import random
 with open('sensors_list.json', 'r') as f:
     sensors = json.load(f)
 
-with open('Airly_API.txt', 'r') as f:
-    api_key = f.read()
+with open('Airly_API.json', 'r') as f:
+    api_keys = json.load(f)
+    n_api_keys = len(api_keys)
 
 date = datetime.datetime.now()
 dir = f'measurements/{date.day}-{date.month}-{date.year}'
@@ -24,11 +25,14 @@ params = {'indexType': 'AIRLY_CAQI',
           'includeWind': 'true'}
 headers = {'Accept': 'application/json',
            'Accept-Language': 'en',
-           'apikey': api_key}
+           'apikey': ''}
 
 
-for id in ids:
+for i, id in enumerate(ids):
+    api_key = api_keys[i % n_api_keys]
+
     params['installationId'] = id
+    headers['apikey'] = api_key
 
     result = requests.get(url, params, headers=headers)
 
