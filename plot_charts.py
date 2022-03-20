7089a0c406d2c0c7ad36053e9870c165eef89171
Paweł Kopel <pawel.kopel2@gmail.com>
2022-03-20 16:47:49 +0100

    Add more charts

--- plot_charts.py
+++ plot_charts.py
@@ -5,9 +5,8 @@ import json
 import matplotlib.pyplot as plt
 
 measurements_dir = './measurements'
-types = {'PM1': 'red', 'PM25': 'green', 'PM10': 'blue'}
 
-measurements = defaultdict(list)
+measurements_dict = defaultdict(list)
 
 for subdir, dirs, files in os.walk(measurements_dir):
     for file in files:
@@ -17,17 +16,36 @@ for subdir, dirs, files in os.walk(measurements_dir):
             if 'history' in values:
                 history = values['history']
                 for value in history:
-                    time = value['fromDateTime'].split('T')[1][:-5]
-                    measurements[time].append(value['values'])
+                    time = value['fromDateTime']
+                    m = {}
+                    for v in value['values']:
+                        m[v['name']] = v['value']
+                    measurements_dict[time].append(m)
+
+measurements_list = []
+
+for _, m in measurements_dict.items():
+    measurements_list += m
+
+
+###################################################################
+# mean ppm to time of day chart
+###################################################################
+
+def drop_dates(measurements: dict) -> dict:
+    new_measurements = defaultdict(list)
+    for k, v in measurements.items():
+        time = k.split('T')[1][:-5]
+        new_measurements[time] += v
+    return new_measurements
 
 
 def mean_of(name: str, values: list) -> float:
     sum = 0
     counter = 0
     for value in values:
-        v = next((v for v in value if v['name'] == name), None)
-        if v is not None:
-            sum += v['value']
+        if name in value:
+            sum += value[name]
             counter += 1
     return sum/counter
 
@@ -39,7 +57,11 @@ def means_of(names: list, values: list) -> dict:
     return result
 
 
-means = {k: means_of(types.keys(), v) for k, v in measurements.items()}
+types = {'PM1': 'red', 'PM25': 'green', 'PM10': 'blue'}
+
+hour_measurements = drop_dates(measurements_dict)
+
+means = {k: means_of(types.keys(), v) for k, v in hour_measurements.items()}
 
 fig, ax = plt.subplots()
 for type, colour in types.items():
@@ -48,6 +70,61 @@ for type, colour in types.items():
     ax.scatter(list(hours), list(values), c=colour, label=type)
 
 ax.legend()
+plt.title('Mean ppm values in 24h')
 plt.xlabel("hour")
-plt.ylabel("ppm")
+plt.ylabel("mean ppm")
 plt.savefig('charts/ppm_to_hour.png')
+
+###################################################################
+# ppm to wind force chart
+###################################################################
+
+
+type_values = defaultdict(list)
+humidity = 'WIND_SPEED'
+
+for v in measurements_list:
+    hum = v.get(humidity)
+    if hum is not None:
+        for type in types.keys():
+            type_value = v.get(type)
+            if type_value is not None:
+                type_values[type].append((hum, type_value))
+
+
+for type, colour in types.items():
+    fig, ax = plt.subplots(figsize=(20, 20))
+    xs = map(lambda x: x[0], type_values[type])
+    ys = map(lambda y: y[1], type_values[type])
+    ax.scatter(list(xs), list(ys), c=colour, label=type)
+    plt.title(f'{type} ppm value to wind speed')
+    plt.xlabel('wind speed')
+    plt.ylabel('ppm')
+    plt.savefig(f'charts/{type}_ppm_to_wind.png')
+
+
+###################################################################
+# ppm to humidity chart
+###################################################################
+
+type_values = defaultdict(list)
+humidity = 'HUMIDITY'
+
+for v in measurements_list:
+    hum = v.get(humidity)
+    if hum is not None:
+        for type in types.keys():
+            type_value = v.get(type)
+            if type_value is not None:
+                type_values[type].append((hum, type_value))
+
+
+for type, colour in types.items():
+    fig, ax = plt.subplots(figsize=(20, 20))
+    xs = map(lambda x: x[0], type_values[type])
+    ys = map(lambda y: y[1], type_values[type])
+    ax.scatter(list(xs), list(ys), c=colour, label=type)
+    plt.title(f'{type} ppm value to humidity')
+    plt.xlabel('humidity [%]')
+    plt.ylabel('ppm')
+    plt.savefig(f'charts/{type}_ppm_to_humidity.png')
