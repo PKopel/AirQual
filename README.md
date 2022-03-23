c918935655a77e646747ae1e8052beca88ba2a54
Paweł Kopel <pawel.kopel2@gmail.com>
2022-03-23 01:29:26 +0100

    Try to optimize data size

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Scripts downloading air quality data from [Airly](https://developer.airly.org/en
 1. Create file `Airly_API.json` containing your Airly API key(s) in a json array (eg. `[ "<API key>" ]`)
 2. Save list of sensors to query in `sensors_list.json` (as a JSON list of objects with at least `'id'` field)
 3. Run `python get_measurements.py` to download and save current measurmenets
+
+## Flat Github
+View measurement data on [flatgithub.com/PKopel/AirQual](https://flatgithub.com/PKopel/AirQual)
--- generate_csv.py
+++ generate_csv.py
@@ -1,78 +1,88 @@
-from collections import defaultdict
 import os
 import json
-import matplotlib.pyplot as plt
 import pandas as pd
-import datetime
-from numba import jit
 from tqdm import tqdm
 
-    
+SENSORS_FILE = './sensors_list.json'
+MEASUREMENTS_CSV = './measurements/all_measurements.csv'
+MEASUREMENTS_DIR = './measurements'
+COLUMNS = ["sensorId", "latitude", "longitude", "elevation", "fromDateTime", "tillDateTime",
+           "PM1", "PM25", "PM10", "pressure", "humidity", "temperature", "wind_speed", "wind_bearing"]
+
+
 def make_measurements_csv(measurements_dir):
-    columns = ["sensorId","latitude","longitude","elevation","fromDateTime","tillDateTime","PM1","PM25","PM10","pressure","humidity","temperature","wind_speed","wind_bearing"]
     data = []
-    counter = 0
-    
+
     sensor_data = {}
 
     with open('./sensors_list.json', 'r') as f:
         sensor_json = json.load(f)
         for sensor in sensor_json:
-            sensor_data[sensor["id"]] = sensor    
-    
+            sensor_data[sensor["id"]] = sensor
+
     print("generating csv")
-    for subdir, dirs, files in tqdm(list(os.walk(measurements_dir))):
+    for subdir, _, files in tqdm(list(os.walk(measurements_dir))):
         for file in tqdm(files):
-            file_path = os.path.join(subdir, file)
-            with open(file_path, 'r') as f:
-                values = json.load(f)
-                if 'history' in values:
-                    history = values['history']
-                    for value in history:
-                        
+            if 'tar.gz' not in file:
+                file_path = os.path.join(subdir, file)
+                with open(file_path, 'r') as f:
+                    values = json.load(f)
+                    if 'history' in values:
+                        history = values['history']
+                        for value in history:
 
-                        new_entry = [None for _ in columns]
+                            new_entry = [None for _ in COLUMNS]
 
-                        sensor_id = int(file.split('.')[0])
-                        new_entry[columns.index("sensorId")] = sensor_id
-                        new_entry[columns.index("fromDateTime")] = pd.to_datetime(value["fromDateTime"])
-                        new_entry[columns.index("tillDateTime")] = pd.to_datetime(value["tillDateTime"])
-                        if(sensor_id in sensor_data):
-                            new_entry[columns.index("latitude")] = sensor_data[sensor_id]["location"]["latitude"]
-                            new_entry[columns.index("longitude")] = sensor_data[sensor_id]["location"]["longitude"]
-                            new_entry[columns.index("elevation")] = sensor_data[sensor_id]["elevation"]
-                        
-                        m = {}
-                        for v in value['values']:
-                            m[v['name']] = v['value']
-                            
-                        if("PM1" in m):
-                            new_entry[columns.index("PM1")] = m["PM1"]
-                        if("PM10" in m):
-                            new_entry[columns.index("PM10")] = m["PM10"]
-                        if("PM25" in m):
-                            new_entry[columns.index("PM25")] = m["PM25"]
-                        if("PRESSURE" in m):
-                            new_entry[columns.index("pressure")] = m["PRESSURE"]
-                        if("HUMIDITY" in m):
-                            new_entry[columns.index("humidity")] = m["HUMIDITY"]
-                        if("TEMPERATURE" in m):
-                            new_entry[columns.index("temperature")] = m["TEMPERATURE"]
-                        if("WIND_SPEED" in m):
-                            new_entry[columns.index("wind_speed")] = m["WIND_SPEED"]
-                        if("WIND_BEARING" in m):
-                            new_entry[columns.index("wind_bearing")] = m["WIND_BEARING"]
+                            sensor_id = int(file.split('.')[0])
+                            new_entry[COLUMNS.index("sensorId")] = sensor_id
+                            new_entry[COLUMNS.index("fromDateTime")] = pd.to_datetime(
+                                value["fromDateTime"])
+                            new_entry[COLUMNS.index("tillDateTime")] = pd.to_datetime(
+                                value["tillDateTime"])
+                            if(sensor_id in sensor_data):
+                                new_entry[COLUMNS.index(
+                                    "latitude")] = sensor_data[sensor_id]["location"]["latitude"]
+                                new_entry[COLUMNS.index(
+                                    "longitude")] = sensor_data[sensor_id]["location"]["longitude"]
+                                new_entry[COLUMNS.index(
+                                    "elevation")] = sensor_data[sensor_id]["elevation"]
 
-                        data.append(new_entry)
-    return pd.DataFrame(data = data, columns = columns)
+                            m = {}
+                            for v in value['values']:
+                                m[v['name']] = v['value']
+
+                            if("PM1" in m):
+                                new_entry[COLUMNS.index("PM1")] = m["PM1"]
+                            if("PM10" in m):
+                                new_entry[COLUMNS.index("PM10")] = m["PM10"]
+                            if("PM25" in m):
+                                new_entry[COLUMNS.index("PM25")] = m["PM25"]
+                            if("PRESSURE" in m):
+                                new_entry[COLUMNS.index(
+                                    "pressure")] = m["PRESSURE"]
+                            if("HUMIDITY" in m):
+                                new_entry[COLUMNS.index(
+                                    "humidity")] = m["HUMIDITY"]
+                            if("TEMPERATURE" in m):
+                                new_entry[COLUMNS.index(
+                                    "temperature")] = m["TEMPERATURE"]
+                            if("WIND_SPEED" in m):
+                                new_entry[COLUMNS.index(
+                                    "wind_speed")] = m["WIND_SPEED"]
+                            if("WIND_BEARING" in m):
+                                new_entry[COLUMNS.index(
+                                    "wind_bearing")] = m["WIND_BEARING"]
+
+                            data.append(new_entry)
+    return pd.DataFrame(data=data, columns=COLUMNS)
+
 
 if(__name__ == "__main__"):
-    data = make_measurements_csv('./measurements/raw')
-    print(data)
-    save_path = "./measurements/all_measurements.csv"
-    data.to_csv(save_path)
-    print(f"csv file saved to {save_path}")
-    
+    old_data = pd.read_csv(MEASUREMENTS_CSV)
+    old_data = old_data[COLUMNS]
+    new_data = make_measurements_csv(f'{MEASUREMENTS_DIR}/raw')
 
-                    
-                    
+    data = pd.concat([old_data, new_data])
+
+    data.to_csv(MEASUREMENTS_CSV)
+    print(f"csv file saved to {MEASUREMENTS_CSV}")
